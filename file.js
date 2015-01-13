1991f7aef0956c0c1fffcacc135730dd3ecd421e
soumyas23 <soumy.s@experionglobal.com>
2015-01-13 09:41:27 +0530

    aded

--- file.js
+++ file.js
@@ -1,6 +1,14 @@
-var fs = require('fs');
+/*var fs = require('fs');
 
 fs.readFile('Largest.js', 'utf-8',function (err, data) {
   if (err) throw err;
   console.log(data);
+});*/
+var fs = require('fs');
+var loginObject={};
+loginObject=fs.readFile('user.txt','utf-8',function (err, data) {
+  if (err) throw err;
+ // console.log(data);	
 });
+
+console.log(loginObject);
